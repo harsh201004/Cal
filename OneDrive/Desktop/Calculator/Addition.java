dae4c9bf9fe8c35d2ef2fc83d3441b8297bbe5f7
harsh201004 <harshraghuwanshi2010@gmail.com>
2025-01-08 22:11:24 +0530

    For multiplication

--- OneDrive/Desktop/Calculator/Addition.java
+++
@@ -1,6 +0,0 @@
-public class Addition{
-   	Public static int add(int a, int b){
-	System.out.println(a + b);
-}
-}
-	
---
+++ OneDrive/Desktop/Calculator/Multiply.java
@@ -0,0 +1,6 @@
+public class Multiply{
+   	Public static int mul(int a, int b){
+	System.out.println(a * b);
+}
+}
+	
